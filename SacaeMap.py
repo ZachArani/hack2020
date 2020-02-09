8e3b9b56f0dfe5494e3a1052e31f1986d4579d02
Clayton McFarland <claytonmc150@gmail.com>
2020-02-09 01:26:33 -0600

    added the proper sprites

--- SacaeMap.py
+++ SacaeMap.py
@@ -1,38 +1,42 @@
 import pygame, sys
+import os
+
+
+current_path = os.path.dirname(__file__)
 
 # Constants representing different resouces
-GRASS = 0
-MOUNTAIN1 = 1
-MOUNTAIN2 = 2
-TEMPLE00 = 3
-TEMPLE01 = 4
-TEMPLE02 = 5
-TEMPLE10 = 6
-TEMPLE11 = 7
-TEMPLE12 = 8
-TEMPLE20 = 9
-TEMPLE21 = 10
-TEMPLE22 = 11
-TREE = 12
+COURT = 0
+OUTERCOURT = 1
+CORNER = 2
+HALFCOURT = 3
+TLINE = 4
+EDGE = 5
+RIGHTEDGE = 6
+BOTTOMEDGE = 7
+LEFTEDGE = 8
+BOTTOMLINE = 9
+TRCORNER = 10
+BRCORNER = 11
+BLCORNER = 12
 LEDGE1 = 13
 LEDGE2 = 14
 LEDGE3 = 15
 HUT = 16
 
 terrains = {
-    0: 'GRASS',
-    1: 'MOUNTAIN1',
-    2: 'MOUNTAIN2',
-    3: 'TEMPLE00',
-    4: 'TEMPLE01',
-    5: 'TEMPLE02',
-    6: 'TEMPLE10',
-    7: 'TEMPLE11',
-    8: 'TEMPLE12',
-    9: 'TEMPLE20',
-    10: 'TEMPLE21',
-    11: 'TEMPLE22',
-    12: 'TREE',
+    0: 'COURT',
+    1: 'OUTERCOURT',
+    2: 'CORNER',
+    3: 'HALFCOURT',
+    4: 'TLINE',
+    5: 'EDGE',
+    6: 'RIGHTEDGE',
+    7: 'BOTTOMEDGE',
+    8: 'LEFTEDGE',
+    9: 'BOTTOMLINE',
+    10: 'TRCORNER',
+    11: 'BRCORNER',
+    12: 'BLCORNER',
     13: 'LEDGE1',
     14: 'LEDGE2',
     15: 'LEDGE3',
@@ -41,43 +45,42 @@ terrains = {
 
 # Dictionary linking resouces to textures
 textures = {
-    GRASS: pygame.image.load('Textures/grass.png'),
-    MOUNTAIN1: pygame.image.load('Textures/mountain1.png'),
-    MOUNTAIN2: pygame.image.load('Textures/mountain2.png'),
-    TEMPLE00: pygame.image.load('Textures/temple00.png'),
-    TEMPLE01: pygame.image.load('Textures/temple01.png'),
-    TEMPLE02: pygame.image.load('Textures/temple02.png'),
-    TEMPLE10: pygame.image.load('Textures/temple10.png'),
-    TEMPLE11: pygame.image.load('Textures/temple11.png'),
-    TEMPLE12: pygame.image.load('Textures/temple12.png'),
-    TEMPLE20: pygame.image.load('Textures/temple20.png'),
-    TEMPLE21: pygame.image.load('Textures/temple21.png'),
-    TEMPLE22: pygame.image.load('Textures/temple22.png'),
-    TREE: pygame.image.load('Textures/tree.png'),
-    LEDGE1: pygame.image.load('Textures/ledge1.png'),
-    LEDGE2: pygame.image.load('Textures/ledge2.png'),
-    LEDGE3: pygame.image.load('Textures/ledge3.png'),
-    HUT: pygame.image.load('Textures/hut.png')
+    COURT: pygame.image.load(os.path.join(current_path,'Textures/court.png')),
+    OUTERCOURT: pygame.image.load(os.path.join(current_path,'Textures/outercourt.png')),
+    CORNER: pygame.image.load(os.path.join(current_path,'Textures/corner.png')),
+    HALFCOURT: pygame.image.load(os.path.join(current_path,'Textures/halfcourt.png')),
+    TLINE: pygame.image.load(os.path.join(current_path,'Textures/tline.png')),
+    EDGE: pygame.image.load(os.path.join(current_path,'Textures/edge.png')),
+    LEFTEDGE: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/edge.png')), 90),
+    BOTTOMEDGE: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/edge.png')), 180),
+    RIGHTEDGE: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/edge.png')), 270),
+    BOTTOMLINE: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/tline.png')), 180),
+    TRCORNER: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/corner.png')), 270),
+    BRCORNER: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/corner.png')), 180),
+    BLCORNER: pygame.transform.rotate(pygame.image.load(os.path.join(current_path,'Textures/corner.png')), 90),
+    LEDGE1: pygame.image.load(os.path.join(current_path,'Textures/ledge1.png')),
+    LEDGE2: pygame.image.load(os.path.join(current_path,'Textures/ledge2.png')),
+    LEDGE3: pygame.image.load(os.path.join(current_path,'Textures/ledge3.png')),
+    HUT: pygame.image.load(os.path.join(current_path,'Textures/hut.png'))
 }
 
 # Tilemap
 tilemap = [
-    [MOUNTAIN1, MOUNTAIN2, TEMPLE00, TEMPLE01, TEMPLE02, GRASS, GRASS, TREE, GRASS, MOUNTAIN1, MOUNTAIN2],
-    [GRASS, GRASS, TEMPLE10, TEMPLE11, TEMPLE12, GRASS, GRASS, GRASS, GRASS, GRASS, MOUNTAIN1],
-    [GRASS, GRASS, TEMPLE20, TEMPLE21, TEMPLE22, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [LEDGE1, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS],
-    [LEDGE2, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, TREE, GRASS],
-    [LEDGE3, GRASS, TREE, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, GRASS, HUT]
+    [OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT],
+    [OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT],
+    [OUTERCOURT, CORNER, EDGE, EDGE, EDGE, TLINE, EDGE, EDGE, EDGE, TRCORNER, OUTERCOURT],
+    [OUTERCOURT, LEFTEDGE, COURT, COURT, COURT, HALFCOURT, COURT, COURT, COURT, RIGHTEDGE, OUTERCOURT],
+    [OUTERCOURT, LEFTEDGE, COURT, COURT, COURT, HALFCOURT, COURT, COURT, COURT, RIGHTEDGE, OUTERCOURT],
+    [OUTERCOURT, LEFTEDGE, COURT, COURT, COURT, HALFCOURT, COURT, COURT, COURT, RIGHTEDGE, OUTERCOURT],
+    [OUTERCOURT, LEFTEDGE, COURT, COURT, COURT, HALFCOURT, COURT, COURT, COURT, RIGHTEDGE, OUTERCOURT],
+    [OUTERCOURT, BLCORNER, BOTTOMEDGE, BOTTOMEDGE, BOTTOMEDGE, BOTTOMLINE, BOTTOMEDGE, BOTTOMEDGE, BOTTOMEDGE, BRCORNER, OUTERCOURT],
+    [OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT],
+    [OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT, OUTERCOURT]
 ]
 
 # Walls
-PASSABLE = [MOUNTAIN1, MOUNTAIN2, GRASS, TREE, HUT, TEMPLE21]
-NOT_PASSABLE = [LEDGE1, LEDGE2, LEDGE3, TEMPLE00, TEMPLE01, TEMPLE02, TEMPLE10, TEMPLE11, TEMPLE12, TEMPLE20, TEMPLE21,
-                TEMPLE22]
+PASSABLE = [COURT,OUTERCOURT,CORNER,HALFCOURT,TLINE,EDGE,RIGHTEDGE,BOTTOMEDGE,LEFTEDGE,BOTTOMLINE,TRCORNER,BRCORNER,BLCORNER]
+NOT_PASSABLE = []
 
 # Game Dimensions
 TILESIZE = 64
